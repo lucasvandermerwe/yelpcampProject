22a04fde813d16e00ab5c080f4ca8d8c03a39c3c
Unknown <yelpcampemailreset@gmail.com>
2018-09-28 14:47:17 +0200

    jquery validator plugin added

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,350 +1,84 @@
-//document ready ensures DOM is loaded before our js runs (even if link to js put in head)
 $(document).ready(function() {
     init();
-
+    setBinds();
 });
 
+function setBinds() {
+    $("#newsletterCheckbox").off("click").on("click", newsletterCheckboxChecked);
+    $("#newsletterCheckboxChecked").off("click").on("click", newsletterCheckboxUnchecked);
+}
+
 function init() {
-    validateRegisterForm();
-    validateUserEditForm();
-    validateNewCampground();
-    validateEditCampground();
-    validateNewComment();
-    validateEditComment();
-
-}
-
-
-//Validate registration form
-function validateRegisterForm(errmessage) {
-
-    $("#signUpButton").on("click", function(event) {
-        event.preventDefault();
-        var usernameText = $.trim($("#username").val());
-        var passwordText = $.trim($("#password").val());
-        var firstnameText = $.trim($("#firstName").val());
-        var surnameText = $.trim($("#surname").val());
-        var emailText = $.trim($("#email").val());
-        var image = $.trim($("#image").val());
-
-
-        if (!usernameText) {
-            errmessage = "Please enter a username";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
+    $("#newComment").validate();
+    $("#editComment").validate();
+    $("#userSignUp").validate({
+        rules: {
+            username: "required",
+            firstname: "required",
+            surname: "required",
+            image: "required",
+            password: {
+                required: true,
+                minlength: 5
+            },
+            confirm_password: {
+                required: true,
+                minlength: 5,
+                equalTo: "#password"
+            },
+            required: true,
+            email: {
+                email: true
+            }
+        },
+        messages: {
+            firstname: "Pleae enter a first name",
+            surname: "Please enter your surname",
+            username: "Please enter a username",
+            image: "Please provide an avatar image",
+            required: "Please a provide your password",
+            password: {
+                minlength: "Password needs to be atleast 5 characters long"
+            },
+            confirm_password: {
+                required: "Please a provide your password",
+                minlength: "Password needs to be atleast 5 characters long",
+                equalTo: "Passwords need to match"
+            },
+            email: {
+                required: "Please provide a valid email address",
+                email: "Please provide a valid email address"
+            }
         }
-        //
-        //regex of conditional 5-10 characters
-        if (!passwordText || passwordText.length < 5 || passwordText.length > 9) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Password needs to be between 5 to 10 characters";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!firstnameText) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please enter a first name";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!surnameText) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please enter a surname";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        //regex om email address te validate
-        if (!ValidateEmail(emailText)) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please enter an email address";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-        if (!image) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide an avatar image";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        $("h4.err").remove();
-        $("div.alert.alert-warning").fadeOut(200);
-        $("#userSignUp").submit();
     });
-}
-
-
-
-//Validate update user form
-function validateUserEditForm(errmessage) {
-
-    $("#userUpdateButton").on("click", function(event) {
-        event.preventDefault();
-        var firstnameText = $.trim($("#firstName").val());
-        var surnameText = $.trim($("#surname").val());
-        var emailText = $.trim($("#email").val());
-        var avatar = $.trim($("#image").val());
-
-        if (!firstnameText) {
-            errmessage = "Please enter a first name";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
+    $("#login_form").validate({
+        rules: {
+            username: "required",
+            password: "required"
+        },
+        messages: {
+            username: "Please provide your username",
+            password: "Please provide your password"
         }
-
-        if (!surnameText) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please enter a surname";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        //regex om email address te validate
-        if (!ValidateEmail(emailText)) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please enter an email address";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!avatar) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide an avatar";
-            $("div.alert.alert-warning").fadeIn(200);
-            $("h4.err").remove();
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-        $("h4.err").remove();
-        $("div.alert.alert-warning").fadeOut(200);
-        $("#userUpdate").submit();
     });
+    $("#userUpdate").validate();
+    $("#campgroundNew").validate();
+    $("#editCampground").validate();
 }
 
 
-function ValidateEmail(mail) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($("#email").val())) {
-        return true;
-    }
-    return false;
-}
-
-
-//Validate Campground Forms
-//New Campground
-
-function validateNewCampground(errmessage) {
-    $("#campgroundNewButton").on("click", function(event) {
-        event.preventDefault();
-        var campgroundName = $.trim($("#name").val());
-        var campgroundImage = $.trim($("#imageUrl").val());
-        var campgroundPrice = $.trim($("#price").val());
-        var campgroundDescription = $.trim($("#description").val());
-        var campgroundLocation = $.trim($("#location").val());
-
-        if (!campgroundName) {
-            errmessage = "Please enter a campground name";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>");
-            return
-        }
-
-        if (!campgroundImage) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide an image";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>");
-            return
-        }
-
-        if (!campgroundPrice) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide a price";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!campgroundDescription) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide a description";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!campgroundLocation) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide a location";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        $("h4.err").remove();
-        $("div.alert.alert-warning").fadeOut(200);
-        $("#campgroundNew").submit();
-    });
-
-}
-
-
-//Edit campground
-function validateEditCampground(errmessage) {
-
-    $("#editCampgroundButton").on("click", function(event) {
-        event.preventDefault();
-        var campgroundName = $.trim($("#name").val());
-        var campgroundImage = $.trim($("#imageUrl").val());
-        var campgroundPrice = $.trim($("#price").val());
-        var campgroundDescription = $.trim($("#description").val());
-        var campgroundLocation = $.trim($("#location").val());
-
-        if (!campgroundName) {
-            errmessage = "Please enter a campground name";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>");
-            return
-        }
-
-        if (!campgroundImage) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide an image";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>");
-            return
-        }
-
-        if (!campgroundPrice) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide a price";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!campgroundDescription) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide a description";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        if (!campgroundLocation) {
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeOut(200);
-            errmessage = "Please provide a location";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>")
-            return
-        }
-
-        $("h4.err").remove();
-        $("div.alert.alert-warning").fadeOut(200);
-        $("#editCampground").submit();
-    });
-}
-
-
-//Validate new comment form
-
-function validateNewComment(errmessage) {
-
-    $("#newCommentButton").on("click", function(event) {
-        event.preventDefault();
-        var commentText = $.trim($("#commentText").val());
-
-        if (!commentText) {
-            errmessage = "Please provide a comment";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>");
-            return
-        }
-        $("h4.err").remove();
-        $("div.alert.alert-warning").fadeOut(200);
-        $("#newComment").submit();
-    });
-}
-
-
-//Validate update comment form
-
-
-function validateEditComment(errmessage) {
-
-    $("#editCommentButton").on("click", function(event) {
-        event.preventDefault();
-        var commentText = $.trim($("#commentText").val());
-
-
-        if (!commentText) {
-            errmessage = "Please provide a comment";
-            $("h4.err").remove();
-            $("div.alert.alert-warning").fadeIn(200);
-            $("div.alert.alert-warning").append("<h4 class='err'>" + errmessage + "</h4>");
-            return
-        }
-        $("h4.err").remove();
-        $("div.alert.alert-warning").fadeOut(200);
-        $("#editComment").submit();
-    });
-}
-
-$("#newsletterCheckbox").on("click", function() {
+function newsletterCheckboxChecked(e) {
+    e.stopPropagation();
     $(this).fadeOut(1);
     $("#newsletterCheckboxChecked").fadeIn(1);
     $("#newsletter").prop("checked", true);
-});
+    setBinds();
+}
 
-$("#newsletterCheckboxChecked").on("click", function() {
+function newsletterCheckboxUnchecked(e) {
+    e.stopPropagation();
     $(this).fadeOut(1);
     $("#newsletterCheckbox").fadeIn(1);
     $("#newsletter").prop("checked", false);
-});
+    setBinds();
+}
